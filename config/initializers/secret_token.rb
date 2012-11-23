5a75b98ac45ec4cc5a067b8e7e83d1cbfe5aecba
luojianming <luojmfly@gmail.com>
2012-11-23 10:41:04 +0800

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupdate::Application.config.secret_token = '5454017a8bb97e9338695f8b6d3aa50901c2784f6c799b978d3a8e08c1d14ab6eece3875a8dd0438b036a4fe6e02a85bd28a688e7ed28848784d289d76a53cfe'
